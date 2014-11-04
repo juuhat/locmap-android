00649d25fc68c9e5382fa24d6d91bb3a03a7d378
Juuso Hatakka <rasek24@gmail.com>
2014-11-04 19:20:53 +0200

    track user's current position

--- src/net/locmap/locmap/MapActivity.java
+++ src/net/locmap/locmap/MapActivity.java
@@ -1,7 +1,12 @@
 package net.locmap.locmap;
 
 import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +16,7 @@ import com.google.android.gms.maps.GoogleMap;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -23,12 +29,15 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapActivity extends Activity {
 
     private GoogleMap myMap;
-    private Marker ict;
+    //private ArrayList<Marker> markers;
+    private LocationManager mLocationManager;
     
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
+		
+		initGPS();
 		
 		// Getting Google Play availability status
 		if (serviceAvailable()) {
@@ -48,31 +57,64 @@ public class MapActivity extends Activity {
 		}
 	}
 	
+	private void initGPS() {
+		mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		long time = 10000; //update frequency in milliseconds
+		float dist = 3; //update minimum distance in meters
+	    mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, time, dist, mLocationListener);
+	}
+	
 	private void initMap() {
 		// find map fragment
 		myMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		
 		// show device location
         myMap.setMyLocationEnabled(true);
+
 		// set map type
 		myMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-		// add one marker
-		final LatLng ICT = new LatLng(62.2416223, 25.7597309);
-		ict = myMap.addMarker(new MarkerOptions()
-                          .position(ICT)
-                          .title("JAMK/ICT"));
-		// point to jamk/ict
-		myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ICT, 14));
-		// marker listener
-		myMap.setOnMarkerClickListener(new OnMarkerClickListener() {
-	        @Override
-	        public boolean onMarkerClick(final Marker marker) {
-	        	if (marker.equals(ict)) {
-	        		Toast.makeText(getApplicationContext(), "Marker = " + marker.getTitle(), Toast.LENGTH_SHORT).show();
-	        		return true;
-	        	}
-	        	return false;
-	        }
-		});	
+		
+		// move camera to last known position
+		Location lastLocation = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		if (lastLocation != null) {
+			myMap.moveCamera(CameraUpdateFactory.newLatLngZoom
+					(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()), 14));
+		}
+		
+	}
+	
+	private final LocationListener mLocationListener = new LocationListener() {
+		@Override
+		public void onLocationChanged(Location location) {
+			myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 14));
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+		}
+	};
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 	
 }
