7801149b077872187e57e7743d34fa2d081fe618
Janne Heikkinen <janne.hei17@gmail.com>
2014-11-12 14:42:42 +0200

    Added register functionality. Needs error handling and better visualization when successful

--- src/net/locmap/locmap/RegisterActivity.java
+++ src/net/locmap/locmap/RegisterActivity.java
@@ -1,9 +1,18 @@
 package net.locmap.locmap;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import net.locmap.locmap.utils.Network;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends Activity {
 
@@ -30,5 +39,54 @@ public class RegisterActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	public void btnRegister(View v) {
+		
+		EditText email = (EditText) findViewById(R.id.editRegisterEmail);
+		EditText user = (EditText) findViewById(R.id.editRegisterUser);
+		EditText password = (EditText) findViewById(R.id.editRegisterPassword);
+		String[] params = {email.getText().toString() , user.getText().toString(), password.getText().toString()};
+		
+		new Register().execute(params);
+	}
+	
+    /**
+     * Async task for register.
+     * Needs three parameters:
+     * 
+     * 1. parameter: Email
+     * 2. parameter: Username
+     * 3. parameter: Password
+     */
+	public class Register extends AsyncTask<String, Void, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			if (params.length < 3)
+				return "";
+			
+			String json = "";
+			JSONObject jsonObj = new JSONObject();
+			// TODO : Add valid for email and stuff ??
+			try {
+				jsonObj.accumulate("email", params[0]);
+				jsonObj.accumulate("username", params[1]);
+				jsonObj.accumulate("password", params[2]);
+				json = jsonObj.toString();
+			} catch (JSONException e) {
+				Log.d("JSON convert", "String to JSON fail @ register");
+			}
+			
+			return Network.Post(Network.registerUrl, json);
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			TextView registerResult = (TextView) findViewById(R.id.txtRegisterResult);
+			registerResult.setText(result);
+		}
+		
 	}
 }
---
+++ src/net/locmap/locmap/utils/Network.java
@@ -0,0 +1,93 @@
+package net.locmap.locmap.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.util.Log;
+
+/**
+ * Class that takes care all the network traffic.
+ * Sends and receives stuff from locmap API
+ * @author Janne Heikkinen
+ */
+public class Network {
+
+	public final static String base = "http://api.locmap.net/v1/";
+	public final static String registerUrl = base + "auth/register";
+	
+	/**
+	 * Sends HTTP POST request, returns the response
+	 * @param url where to send request
+	 * @param json data to send
+	 * @return response
+	 */
+	public static String Post(String url, String json) {
+		return Post(url, json, "");
+	}
+	
+	
+	/** 
+	 * Sends HTTP POST request, returns the response
+	 * @param url where to send post request
+	 * @param json data to send
+	 * @param token Authentication key
+	 * @return Response
+	 */
+	public static String Post(String url, String json, String token) {
+		String result = "";
+		InputStream inputstream = null;
+		StringEntity se;
+		try {
+			HttpClient client = new DefaultHttpClient();
+			HttpPost post = new HttpPost(url);
+			se = new StringEntity(json);
+			post.setEntity(se);
+			post.setHeader("Accept", "application/json");
+	        post.setHeader("Content-type", "application/json");
+	        post.setHeader("Authorization", "Bearer " + token);
+
+			HttpResponse response = client.execute(post);
+			
+			inputstream = response.getEntity().getContent();
+			
+			if (inputstream != null)
+				result = convertInputStreamToString(inputstream);
+			else
+				result = "";
+			
+		} catch (ClientProtocolException e) {
+			Log.d("ClientProtocolException", e.getMessage());
+		} catch (IOException e) {
+			Log.d("IOException: ", e.getMessage());
+		}
+		
+		return result;
+	}
+	
+	
+	/**
+	 * @param inputStream to convert
+	 * @return inputStream as string
+	 * @throws IOException if something goes terribly wrong
+	 */
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+ 
+        inputStream.close();
+        return result;
+    }   
+	
+
+}
