1fa8e625e021e2929f213162fd761670bac62990
Juuso Hatakka <rasek24@gmail.com>
2014-11-16 00:51:15 +0200

    update textViews with Location param

--- src/net/locmap/locmap/ShowLocationActivity.java
+++ src/net/locmap/locmap/ShowLocationActivity.java
@@ -1,15 +1,29 @@
 package net.locmap.locmap;
 
+import net.locmap.locmap.models.Location;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 public class ShowLocationActivity extends Activity {
-
+	
+	private Location location;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_show_location);
+
+		this.location = (Location) getIntent().getExtras().getParcelable("location");
+		updateTextViews();
 	}
+	
+	private void updateTextViews() {
+		TextView title = (TextView) findViewById(R.id.txtShowLocationTitle);
+		TextView description = (TextView) findViewById(R.id.txtShowLocationDescription);
+		
+		title.setText(location.getTitle());
+		description.setText(location.getDescription());
+	}
+	
 }
