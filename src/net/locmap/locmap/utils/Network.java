850315947d935eea67aefafec46a389d9031ef7d
rasek <rasek24@gmail.com>
2014-11-28 21:11:36 +0200

    added method for GET requests

--- src/net/locmap/locmap/utils/Network.java
+++ src/net/locmap/locmap/utils/Network.java
@@ -10,6 +10,7 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.HttpMultipartMode;
@@ -23,8 +24,9 @@ import android.net.NetworkInfo;
 import android.util.Log;
 
 /**
- * Class that takes care all the network traffic.
- * Sends and receives stuff from locmap API
+ * Class that takes care all the network traffic. Sends and receives stuff from
+ * locmap API
+ * 
  * @author Janne Heikkinen
  */
 public class Network {
@@ -32,12 +34,13 @@ public class Network {
 	public final static String base = "http://api.locmap.net/v1/";
 	public final static String registerUrl = base + "auth/register";
 	public final static String loginUrl = base + "auth/login";
-	public final static String locationsUrl = base + "locations";
-	public final static String imagesUrl = base + "images";
-	
-	
+	public final static String locationsUrl = base + "locations/";
+	public final static String imagesUrl = base + "images/";
+	public final static String usersUrl = base + "users/";
+
 	/**
 	 * Sends HTTP POST request, returns the response
+	 * 
 	 * @param url where to send request
 	 * @param json data to send
 	 * @return response
@@ -45,23 +48,24 @@ public class Network {
 	public static Response Post(String url, String json) {
 		return Post(url, json, "");
 	}
-	
-	
+
 	/**
 	 * Checks whether device is connected to the Internet or not
+	 * 
 	 * @return
 	 */
 	public static boolean isNetworkAvailable(Activity act) {
-	    ConnectivityManager connectivityManager 
-	          = (ConnectivityManager) act.getSystemService(Context.CONNECTIVITY_SERVICE);
-	    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-	    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+		ConnectivityManager connectivityManager = (ConnectivityManager) act
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetworkInfo = connectivityManager
+				.getActiveNetworkInfo();
+		return activeNetworkInfo != null && activeNetworkInfo.isConnected();
 	}
-	
-	
-	/** 
+
+	/**
 	 * Sends HTTP POST request, returns the response
-	 * @param url where to send post request
+	 * 
+	 * @param url  where to send post request
 	 * @param json data to send
 	 * @param token Authentication key
 	 * @return Response
@@ -76,8 +80,8 @@ public class Network {
 			se = new StringEntity(json);
 			post.setEntity(se);
 			post.setHeader("Accept", "application/json");
-	        post.setHeader("Content-type", "application/json");
-	        post.setHeader("Authorization", "Bearer " + token);
+			post.setHeader("Content-type", "application/json");
+			post.setHeader("Authorization", "Bearer " + token);
 
 			HttpResponse response = client.execute(post);
 
@@ -85,48 +89,86 @@ public class Network {
 			inputstream = response.getEntity().getContent();
 			if (inputstream != null)
 				body = convertInputStreamToString(inputstream);
-			
+
 			res.setHeaders(response.getAllHeaders());
 			res.setStatusCode(response.getStatusLine().getStatusCode());
 			res.setBody(body);
-			
+
 		} catch (ClientProtocolException e) {
 			Log.d("ClientProtocolException", e.getMessage());
 		} catch (IOException e) {
 			Log.d("IOException: ", e.getMessage());
 		}
-		
+
 		return res;
 	}
-	
-	
+
 	/**
 	 * Sends HTTP POST Multipart request, returns response
+	 * 
 	 * @param url where to send post request
 	 * @param file to send
 	 * @param location ObjectId where this image will be attached
 	 * @param token Authentication key
 	 * @return Response
 	 */
-	public static Response Post(String url, File file, String location, String token) {
+	public static Response Post(String url, File file, String location,
+			String token) {
+		Response res = new Response();
+		InputStream inputstream = null;
+
+		try {
+			HttpClient client = new DefaultHttpClient();
+			HttpPost post = new HttpPost(url);
+			post.setHeader("Authorization", "Bearer " + token);
+
+			// Build multipart entity
+			MultipartEntityBuilder entityBuilder = MultipartEntityBuilder
+					.create();
+			entityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+			entityBuilder.addTextBody("location", location);
+			entityBuilder.addBinaryBody("image", file);
+
+			HttpEntity entity = entityBuilder.build();
+			post.setEntity(entity);
+
+			HttpResponse response = client.execute(post);
+
+			String body = "";
+			inputstream = response.getEntity().getContent();
+			if (inputstream != null)
+				body = convertInputStreamToString(inputstream);
+
+			res.setHeaders(response.getAllHeaders());
+			res.setStatusCode(response.getStatusLine().getStatusCode());
+			res.setBody(body);
+
+		} catch (ClientProtocolException e) {
+			Log.d("ClientProtocolException", e.getMessage());
+		} catch (IOException e) {
+			Log.d("IOException: ", e.getMessage());
+		}
+
+		return res;
+
+	}
+
+	/**
+	 * Send HTTP GET request
+	 * @param url
+	 * @return response
+	 */
+	public static Response Get(String url) {
+		HttpClient client = new DefaultHttpClient();
 		Response res = new Response();
 		InputStream inputstream = null;
 		
 		try {
-			HttpClient client = new DefaultHttpClient();
-			HttpPost post = new HttpPost(url);
-	        post.setHeader("Authorization", "Bearer " + token);
-			
-			//Build multipart entity
-			MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create();
-			entityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
-			entityBuilder.addTextBody("location", location);
-			entityBuilder.addBinaryBody("image", file);
-			
-			HttpEntity entity = entityBuilder.build();
-			post.setEntity(entity);
-			
-			HttpResponse response = client.execute(post);
+			HttpGet get = new HttpGet(url);
+			get.setHeader("Accept", "application/json");
+			get.setHeader("Content-type", "application/json");
+
+			HttpResponse response = client.execute(get);
 			
 			String body = "";
 			inputstream = response.getEntity().getContent();
@@ -142,27 +184,26 @@ public class Network {
 		} catch (IOException e) {
 			Log.d("IOException: ", e.getMessage());
 		}
-		
+
 		return res;
-		
 	}
-	
-	
+
 	/**
 	 * @param inputStream to convert
 	 * @return inputStream as string
 	 * @throws IOException if something goes terribly wrong
 	 */
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while((line = bufferedReader.readLine()) != null)
-            result += line;
- 
-        inputStream.close();
-        return result;
-    }   
-	
+	private static String convertInputStreamToString(InputStream inputStream)
+			throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(
+				new InputStreamReader(inputStream));
+		String line = "";
+		String result = "";
+		while ((line = bufferedReader.readLine()) != null)
+			result += line;
+
+		inputStream.close();
+		return result;
+	}
 
 }
