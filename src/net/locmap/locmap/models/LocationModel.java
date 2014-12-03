ce09556c602197408dc58e37feb0d417e0b7d726
Juuso Hatakka <rasek24@gmail.com>
2014-12-03 14:39:17 +0200

    fixed setLongitude method

--- src/net/locmap/locmap/models/LocationModel.java
+++ src/net/locmap/locmap/models/LocationModel.java
@@ -106,19 +106,19 @@ public class LocationModel implements Parcelable {
 		this.description = description;
 	}
 
-	public Float getLatitude() {
+	public float getLatitude() {
 		return latitude;
 	}
 
-	public void setLatitude(Float latitude) {
+	public void setLatitude(float latitude) {
 		this.latitude = latitude;
 	}
 
-	public Float getLongitude() {
+	public float getLongitude() {
 		return longitude;
 	}
 
-	public void setFloat(Float longitude) {
+	public void setLongitude(float longitude) {
 		this.longitude = longitude;
 	}
 
